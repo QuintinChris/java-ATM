8f080892098d27b7eb31e3870d1060d97d774e60
QuintinChris <cwq23drexel@gmail.com>
2020-06-12 16:09:56 -0400

    set up test accounts

---
+++ src/src/Keypad.java
@@ -0,0 +1,21 @@
+package src;
+
+/**
+ *
+ * @author cwq23
+ */
+import java.util.Scanner; // program uses Scanner to obtain user input
+
+public class Keypad {
+   private Scanner input; // reads data from the command line
+                         
+   // no-argument constructor initializes the Scanner
+   public Keypad() {
+      input = new Scanner(System.in);    
+   } 
+
+   // return an integer value entered by user 
+   public int getInput() {
+      return input.nextInt(); // we assume that user enters an integer
+   } 
+} 
